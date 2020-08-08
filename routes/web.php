a3d2952c8723ecb7cc3762f50fdef15a37f0804a
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-08-08 09:32:47 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','TaskController@welcome');
+//Route::get('/','TaskController@welcome');
 
 //view
 Route::get('/','TaskController@listTask');
